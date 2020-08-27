fef57d64e1d84709e2710b8b96cbe7ebc6a05751
ThereseSB <thereseskaar@hotmail.com>
2020-08-27 12:47:02 +0200

    add routes for individual post, and generate the post-page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Hello and welcome to my blog. This is a little blog about me and some of the things that are happening in my life.";
 const aboutContent = "My name is Therese and I live in bergen Norway. I am currently taking a course in full-stack web development where I am learning about how to build web-apps. The creative proccess is fun and it is motivating to see my skill levels increase with each project. In the near future I hope to start building my own project with the React-framework. Maybe I will tell You about it in my blog? 😉";
@@ -43,19 +44,30 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
+
 app.post("/compose", function(req, res) {
   const post = {
     title: req.body.postTitle,
     content: req.body.postContent
-  }
+  };
 
   posts.push(post);
 
   res.redirect("/");
-})
+});
 
 
-
+app.get("/posts/:blogpost", function(req, res) {
+  const requestedTitle = _.kebabCase(req.params.blogpost.toLowerCase());
+  posts.forEach(function(post) {
+    const postTitle = _.kebabCase(post.title.toLowerCase());
+    if (postTitle === requestedTitle) {
+      res.render("post", {
+        myPost: post
+      });
+    };
+  });
+});
 
 
 
